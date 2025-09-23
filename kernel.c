679666edaf340e17240efc0eb7d7be3d5f703b0b
zylonkcompany <168353509+zylonkcompany@users.noreply.github.com>
2025-09-23 19:41:39 -0300

    NyOS!

--- kernel.c
+++ kernel.c
@@ -7,7 +7,7 @@ int init(){
     HalPrintString("Preparing the system...\n");
     HalPrintString("Initializing...\n");
     int *sys_res = HalMalloc(0x200000);
-    if(*sys_res == NULL){
+    if(sys_res == NULL){
         HalPanicMessage("Fault in alloc system reserved memory!\n");
         return ERROR;
     }
